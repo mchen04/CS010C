624130cc3396c1108cd18b935c8f1373f5bbdaad
mchen04 <michaelluochen1@gmail.com>
2023-01-21 16:56:03 -0800

    Prog 1

---
+++ 2.28/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Node {
+    string payload;
+    Node* next;
+};
+
+Node* newNode(string payload) {
+   Node* temp = new Node ; //creating a new node
+   temp -> payload = payload ; //assigning data variable
+   temp -> next = nullptr ; //remove dangling pointer
+   return temp ;
+}
+
+Node* loadGame(int n, vector<string> names) {
+    Node* head = nullptr;
+    Node* prev = nullptr;
+    string name;
+
+    for (int i = 0; i < n; ++i) {
+        name = names.at(i);
+        if (head == nullptr) {
+            head = newNode(name); // initialize head specially
+            prev = head ; //sets prev to head to use to add more nodes later
+        } else {
+            prev->next = newNode(name); //adds a new node to the list
+            prev = prev -> next ; //moves prev over to the address of the new node
+
+        }
+    }
+
+    if (prev != nullptr) {
+        prev -> next = head ; //makes circular
+    }
+    return head;
+}
+
+void print(Node* start) { // prints list
+    Node* curr = start;
+    while (curr != nullptr) {
+        cout << curr->payload << endl;
+        curr = curr->next;
+        if (curr == start) {
+            break; // exit circular list
+        }
+    }
+}
+
+Node* runGame(Node* start, int k) { // josephus w circular list, k = num skips
+    Node* curr = start;
+    Node* prev = curr;
+    while (curr -> next != curr) { // exit condition, last person standing
+        for (int i = 0; i < k; ++i) { // find kth node
+            prev = curr ; //reassigns prev
+            curr = curr -> next ; //reassigns curr
+        }
+
+        prev -> next = curr -> next ; // fixes pointers around soon deleted node 
+        delete curr ; //deletes node 
+        curr = prev -> next ; //points curr to a node
+    }
+
+    return curr; // last person standing
+}
+
+/* Driver program to test above functions */
+int main() {
+    int n=1, k=1 ; // n = num names; k = num skips (minus 1)
+    string name;
+    vector<string> names;
+
+    // get inputs
+    cin >> n >> k;
+    while (cin >> name && name != ".") { names.push_back(name); } // EOF or . ends input
+
+    // initialize and run game
+    Node* startPerson = loadGame(n, names);
+    Node* lastPerson = runGame(startPerson, k);
+
+    if (lastPerson != nullptr) {
+        cout << lastPerson->payload << " wins!" << endl;
+    } else {
+        cout << "error: null game" << endl;
+    }
+
+    return 0;
+}
+
